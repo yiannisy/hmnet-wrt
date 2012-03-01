b342a3a7155e1046e1252c245c95c890d0b301c2
Yiannis Yiakoumis <yiannisy@stanford.edu>
2012-03-01 14:56:06 -0800

    Get more details for bitrate logging.

--- hoomenets/files/lib/hmnet/get_bitrates.sh
+++ hoomenets/files/lib/hmnet/get_bitrates.sh
@@ -11,18 +11,18 @@ SSH_IDENTITY_FILE="/root/.ssh/airport_rsa"
 i=0
 SECS_PER_FILE=3600
 echo "Start dumping bitrates in $LOGFILE"
-echo "date,mac,inactive,tx_bitrate,rx_bitrate" > $LOGFILE
+echo "date,mac,inactive,rx_bytes,rx_packets,tx_bytes,tx_packets,tx_retries,tx_failed,tx_bitrate,rx_bitrate" > $LOGFILE
 while true;
 do
 	ts=`date +"%Y%m%d%H%M%S"`
-	iw dev ${IF_WIRELESS} station dump | awk '{if($1=="Station"){printf("%s,%s,","'$ts'",$2)}else if($1=="inactive" && $2=="time:"){printf("%s%s,",$3,$4)}else if($1=="tx" && $2=="bitrate:"){printf("%s,",$3)}else if($1=="rx" && $2=="bitrate:"){printf("%s\n",$3)}}' >> $LOGFILE
+	iw dev ${IF_WIRELESS} station dump | awk '{if($1=="Station"){printf("%s,%s,","'$ts'",$2)}else if($1=="inactive" && $2=="time:"){printf("%s%s,",$3,$4)}else if($1=="rx" && $2=="bytes:"){printf("%s,",$3)}else if($1=="rx" && $2=="packets:"){printf("%s,",$3)}else if($1=="tx" && $2=="bytes:"){printf("%s,",$3)}else if($1=="tx" && $2=="packets:"){printf("%s,",$3)}else if($1=="tx" && $2=="retries:"){printf("%s,",$3)}else if($1=="tx" && $2=="failed:"){printf("%s,",$3)}else if($1=="tx" && $2=="bitrate:"){printf("%s,",$3)}else if($1=="rx" && $2=="bitrate:"){printf("%s\n",$3)}}' >> $LOGFILE
 	sleep 1;
 	let "i+=1"
 	if [ $i == $SECS_PER_FILE ]; then
 		cp $LOGFILE ${LOGFILE}.${ts} 
-		scp -i ${$SH_IDENTITY_FILE} {LOGFILE}.${ts} apreport@homenets.stanford.edu:~/bitrate-logs/
+		scp -i ${SSH_IDENTITY_FILE} ${LOGFILE}.${ts} apreport@homenets.stanford.edu:~/bitrate-logs/
 		rm ${LOGFILE}.${ts}
-		echo "date,mac,inactive,tx_bitrate,rx_bitrate" > $LOGFILE
+		echo "date,mac,inactive,rx_bytes,rx_packets,tx_bytes,tx_packets,tx_retries,tx_failed,tx_bitrate,rx_bitrate" > $LOGFILE
 		i=0
 	fi
 done
